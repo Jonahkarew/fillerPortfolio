d82abd8b47da1110e5b940e483749842b16d6878
Jonah Karew <jonahkarew@gmail.com>
2020-08-13 18:55:13 -0400

    added dot canvas component and loader animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 import Header from './components/Header';
 import About from './components/About';
 import Work from './components/work';
 import Contact from './components/Contact';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import DotCanvas from './components/DotCanvas/';
 
 function App() {
+
+  useEffect(() =>{
+    const loader = document.getElementById('loader');
+    if(loader){
+      loader.classList.add('hidden')
+    }
+
+    setTimeout(() =>{
+      loader.outerHTML = ''
+    }, 6000)
+  })
+  
   return (
-    <div className="App">
+    <React.Fragment>
+      <DotCanvas />
+         <div className="App">
       <Navbar></Navbar>
       <div className="App-header">
       <Header></Header>
@@ -25,6 +40,8 @@ function App() {
         <Contact></Contact>
       </div>
     </div>
+    </React.Fragment>
+ 
   );
 }
 

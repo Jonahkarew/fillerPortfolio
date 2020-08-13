8a3502fce6d0f9404472234689d48a61f18944be
Jonah Karew <jonahkarew@gmail.com>
2020-08-12 21:33:35 -0400

    template setup and initialization

---
+++ src/yourdata.js
@@ -0,0 +1,70 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'Jonah Karew.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 My Name is Jonah Karew.',
+                        //Line 2 For Header
+                        'Web Developer',
+                        //Line 3 For Header
+                        'from New Jersey'
+    ],
+        //Contact Email
+        contactEmail:'jonahkarew@gmail.com',
+        // Add Your About Text Here
+        abouttext: `Hi, I'm Jonah. I'm pretty new to the world of full stack development, but what I lack in experience I make up for in pure determination.`,
+        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'Project One', //Project Title - Add Your Project Title Here
+             service:'UI/UX Design', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             //Project URL - Add Your Project Url Here
+             url:'http://chetanverma.com/'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project Two',
+                service: 'Website Development',
+                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+            },
+            { 
+                id: 3,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project Three',
+                service: 'Web App',
+                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+            },
+            {
+                id: 4,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project Four',
+                service: 'Branding',
+                imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+           }
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/Jonahkarew'},
+        ]
+    }
